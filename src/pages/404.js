6306541dcf8ba2a5db8e6af3b12f78a8fa41489e
benomatis <benomatis@gmail.com>
2022-04-24 22:01:35 +0200

    removed some redundant components + updated the 404 page + trying to make sure hover color doesn't appear for active class on mobile

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
-const FourOFour = () => (
-  <div>oops...</div>
+import Layout from '../components/layout'
+import Seo from '../components/seo'
+
+const FourOFour = ({ location }) => (
+  <Layout location={location}>
+    <Seo
+      title="Az oldal nem található"
+      description="Az oldal nem található"
+    />
+    <div style={{ textAlign: 'center', padding: 20 }}>Hoppá, valamit elírhattál. Sajna ilyen oldalunk nincsen...</div>
+  </Layout>
 )
 
 export default FourOFour
